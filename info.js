f4d8dfa80ff837c1e0fa0eb2060465833f46cbdb
kimberlycomeghod <143330650+kimberlycomeghod@users.noreply.github.com>
2023-11-26 21:34:01 +0800

    initial change

---
+++ info.js
@@ -0,0 +1,48 @@
+export const users = [
+    {
+      nickName:'kim',
+      name: {
+        firstName:'KIMBERLY',
+        lastName:'COMEGHOD',
+      },
+      course:'BSIT',
+      year:'3',
+      
+    },
+    {
+      nickName:'shen',
+      name: {
+        firstName:'SHERILYN',
+        lastName:'PAQUINOL',
+      },
+      course:'CRIM',
+      year:'2',
+    },
+    {
+      nickName:'laiza',
+      name: {
+        firstName:'MA LAIZA',
+        lastName:'HANGAD',
+      },
+      course:'BSAgEng’g',
+      year:'1',
+    },
+    {
+      nickName:'carlo',
+      name: {
+        firstName:'JOHN CARLO',
+        lastName:'RAZON',
+      },
+      course:'BEEd',
+      year:'3',
+    },
+    {
+      nickName:'nal',
+      name: {
+        firstName:'ANALIE',
+        lastName:'BARBANIDA',
+      },
+      course:'BSA',
+      year:'4',
+    },
+  ];
